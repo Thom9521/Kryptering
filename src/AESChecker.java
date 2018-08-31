b571928c88e63ad53e73321dc83d6648c9470b72
Thom9521 <Thom9521@edu.easj.dk>
2018-08-31 14:20:57 +0200

    Initial commit

---
+++ src/AESChecker.java
@@ -0,0 +1,13 @@
+public class AESChecker {
+
+    public static void main(String[] args) throws Exception {
+
+        String password = "mypassword";
+        String passwordEnc = AESkryptering.encrypt(password);
+        String passwordDec = AESkryptering.decrypt(passwordEnc);
+
+        System.out.println("Plain Text : " + password);
+        System.out.println("Encrypted Text : " + passwordEnc);
+        System.out.println("Decrypted Text : " + passwordDec);
+    }
+}
